e1eadb135a7d361c4c983f5137c53a0e8adf8db5
blacklizard <jeevanlx@gmail.com>
2018-04-20 13:34:23 +0800

    add born outside malaysia

--- test/nric.test.js
+++ test/nric.test.js
@@ -43,6 +43,11 @@ describe('Given a NRIC number', () => {
       expect(nric.getState().toLowerCase()).to.be.equal('perak');
     });
 
+    it('should return a valid state', () => {
+      const nric = new NRICUtil('800709-71-5827');
+      expect(nric.getState().toLowerCase()).to.be.equal('a person born outside malaysia prior to 2001 excluding those born abroad without holding high quality identity card');
+    });
+
     it('gender should be male', () => {
       const nric = new NRICUtil('800709-08-5827');
       expect(nric.getGender()).to.be.equal('male');
@@ -64,7 +69,7 @@ describe('Given a NRIC number', () => {
 
     describe('with wrong state information', () => {
       it('should be validated as false', () => {
-        const nric = new NRICUtil('000712-99-5827');
+        const nric = new NRICUtil('000712-97-5827');
         expect(nric.isValid()).to.be.equal(false);
       });
     });
